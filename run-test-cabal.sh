a2039f7496ba48c3fd028d7827e6da61089013bf
Andreas Abel <andreas.abel@ifi.lmu.de>
2024-11-18 16:03:01 +0100

    run-test-FOO.sh: forward all arguments to plt-test-lab4

--- run-test-cabal.sh
+++ run-test-cabal.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env sh
 
+# Programming Language Technology (Chalmers DAT151 / GU DIT231)
+# (C) 2022-24 Andreas Abel
+# All rights reserved.
+
 if [ "$1" == "" -o  "$1" == "-h" -o "$1" == "--help" ]; then
   echo "PLT lab 4 testsuite runner"
-  echo "usage: $0 DIRECTORY"
+  echo "usage: $0 [OPTIONS] DIRECTORY"
+  echo "Takes the same options as plt-test-lab4:"
+  cabal run plt-test-lab4
   exit 1
 fi
 
-cabal run plt-test-lab4 -- "$1"
+cabal run plt-test-lab4 -- "$@"
 
 # EOF
--- run-test-stack.sh
+++ run-test-stack.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env sh
 
+# Programming Language Technology (Chalmers DAT151 / GU DIT231)
+# (C) 2022-24 Andreas Abel
+# All rights reserved.
+
 if [ "$1" == "" -o  "$1" == "-h" -o "$1" == "--help" ]; then
   echo "PLT lab 4 testsuite runner"
-  echo "usage: $0 DIRECTORY"
+  echo "usage: $0 [OPTIONS] DIRECTORY"
+  echo "Takes the same options as plt-test-lab4:"
+  stack run
   exit 1
 fi
 
-stack run -- "$1"
+stack run -- "$@"
 
 # EOF
